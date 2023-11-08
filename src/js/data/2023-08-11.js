195659497baa8db012daecdc399d1aa95c4fccc0
Deny Satrio <denysatrio1@gmail.com>
2023-11-08 17:47:30 +0700

    update imge url

--- src/js/data/2023-08-11.js
+++ src/js/data/2023-08-11.js
@@ -6,247 +6,247 @@ dataSet[dataSetVersion].options = [];
 dataSet[dataSetVersion].characterData = [
   {
     "name": "Leotan",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Teddygoo",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Naga",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Ben",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Omega",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Wisnuars",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Baron",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Elds",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Mino",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Fyn",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Pewchan",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Satria",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Hashiren",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "AnvlLuck",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Amo",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Felisyus",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "HoloBaby",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Steno",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Kacang Beruntung",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "O_Kai",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Ilham",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Iam",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Curtsey",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Shiraa",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "vsapik",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "DarmaS",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "SnoopCube",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "ライト",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Fabrizio",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "ArkaN",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Kesuma",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Ryuki 69",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Bruh",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Telo Archer",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "andriascn",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "jjQlLlLq",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "naffasan",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "nathanaelsugiharto",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Cheesey",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Ruffiaant (a ant)",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "VTifand",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Mashiro",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Mie Ayam",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Jacob",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Arima",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Felisyus",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Geroo",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Senate",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   },
   {
     "name": "Zyan",
-    "img": "CsRUFfu.png",
+    "img": "H5plwaY.png",
     "opts": {}
   }
 ];
