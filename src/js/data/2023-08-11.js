694ad4c6d85793929b75d9e226b12217c57a8366
Deny Satrio <denysatrio1@gmail.com>
2023-11-08 16:49:32 +0700

    Initial Commit

---
+++ src/js/data/2023-08-11.js
@@ -0,0 +1,252 @@
+dataSetVersion = "2023-08-11"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    "name": "Leotan",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Teddygoo",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Naga",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Ben",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Omega",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Wisnuars",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Baron",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Elds",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Mino",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Fyn",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Pewchan",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Satria",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Hashiren",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "AnvlLuck",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Amo",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Felisyus",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "HoloBaby",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Steno",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Kacang Beruntung",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "O_Kai",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Ilham",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Iam",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Curtsey",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Shiraa",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "vsapik",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "DarmaS",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "SnoopCube",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "ライト",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Fabrizio",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "ArkaN",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Kesuma",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Ryuki 69",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Bruh",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Telo Archer",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "andriascn",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "jjQlLlLq",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "naffasan",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "nathanaelsugiharto",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Cheesey",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Ruffiaant (a ant)",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "VTifand",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Mashiro",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Mie Ayam",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Jacob",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Arima",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Felisyus",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Geroo",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Senate",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  },
+  {
+    "name": "Zyan",
+    "img": "CsRUFfu.png",
+    "opts": {}
+  }
+];
